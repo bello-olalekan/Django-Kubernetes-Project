9efaee1b144455b16a5da36d6375105c52442617
bello olalekan <belloolalekan11@yahoo.com>
2022-07-06 22:08:07 +0100

    keys

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('nodes/', views.display_nodes),
+    path('pods/', views.display_pods)
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from kubernetes import client, config
 
 # Create your views here.
+def display_nodes(request):
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    nodes = v1.list_node().items
+    return render(request, 'nodes.html', {'data': nodes})
+
+
